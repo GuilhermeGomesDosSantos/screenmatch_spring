c85bbb67848ff4dd8f113268a94284b56aed1dcf
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-19 14:56:04 -0300

    Buscando dados completos da série

--- screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/ScreenmatchSpringApplication.java
+++ screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -1,16 +1,9 @@
 package br.com.alura.screenmatch_spring;
 
-import br.com.alura.screenmatch_spring.model.DadosEpisodio;
-import br.com.alura.screenmatch_spring.model.DadosSerie;
-import br.com.alura.screenmatch_spring.model.DadosTemporada;
-import br.com.alura.screenmatch_spring.service.ConsumoAPI;
-import br.com.alura.screenmatch_spring.service.ConverteDados;
+import br.com.alura.screenmatch_spring.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchSpringApplication implements CommandLineRunner {
@@ -21,30 +14,7 @@ public class ScreenmatchSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=31df3067");
-//		System.out.println(json);
-//
-//		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		System.out.println("Dados do Episodio");
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=31df3067");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=31df3067");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		System.out.println("Lista de Temporadas:");
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/principal/Principal.java
+++ screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/principal/Principal.java
@@ -1,7 +1,12 @@
 package br.com.alura.screenmatch_spring.principal;
 
+import br.com.alura.screenmatch_spring.model.DadosSerie;
+import br.com.alura.screenmatch_spring.model.DadosTemporada;
 import br.com.alura.screenmatch_spring.service.ConsumoAPI;
+import br.com.alura.screenmatch_spring.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -10,9 +15,22 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String APIKEY ="&apikey=31df3067";
     private ConsumoAPI consumo = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
     public void exibeMenu() {
         System.out.println("Digite o nome da série");
+
         var nomeSerie = leitura.nextLine();
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + APIKEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + APIKEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
     }
 }
