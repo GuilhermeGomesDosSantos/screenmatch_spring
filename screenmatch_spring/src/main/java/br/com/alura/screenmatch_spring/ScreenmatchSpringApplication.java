ce17d4b74aaed312e6b635c79cc00c02faaed205
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-19 09:09:55 -0300

    Modelando dados da série

--- screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/ScreenmatchSpringApplication.java
+++ screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch_spring;
 
+import br.com.alura.screenmatch_spring.model.DadosSerie;
 import br.com.alura.screenmatch_spring.service.ConsumoAPI;
+import br.com.alura.screenmatch_spring.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +17,14 @@ public class ScreenmatchSpringApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Seasons=1&apikey=31df3067");
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=31df3067");
 //		System.out.println(json);
 //
 //		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
