27d760f50998962c17316ada84ea1b476089479b
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-19 15:12:16 -0300

    Trabalhando na coleção de dados

--- screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/principal/Principal.java
+++ screenmatch_spring/src/main/java/br/com/alura/screenmatch_spring/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch_spring.principal;
 
+import br.com.alura.screenmatch_spring.model.DadosEpisodio;
 import br.com.alura.screenmatch_spring.model.DadosSerie;
 import br.com.alura.screenmatch_spring.model.DadosTemporada;
 import br.com.alura.screenmatch_spring.service.ConsumoAPI;
@@ -32,5 +33,15 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j <episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        temporadas.forEach(System.out::println);
     }
 }
